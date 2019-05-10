06d165f56d36933a8322777425cee4c80c6190f6
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-10 03:57:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -62,7 +62,8 @@ def monopoly_with_fourth_tier
 		    :names => {
 		      :reading_railroad => 
 		      {
-
+            "mortgage_value" => "$100",
+            
 		      },
 		      :pennsylvania_railroad => {}, 
 		      :b_and_o_railroad => {},

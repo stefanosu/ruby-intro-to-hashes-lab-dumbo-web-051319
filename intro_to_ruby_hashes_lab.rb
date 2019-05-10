3c74c7bf59bc98bea90834a88c2f687f5041088b
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-10 03:44:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,8 +34,12 @@ def monopoly_with_third_tier
 		  {
 		    :pieces => 4, 
 		    :names => {}, 
-		    :rent_in_dollars => {},
-		  } 
+		    :rent_in_dollars => 
+		    {
+		      :one_piece_owned => 25,
+		      :
+		    },
+		  }
 		}  
   
   		
